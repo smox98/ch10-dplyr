19415f3f343d7896402a8a84fda4fc530ed42c46
Stephen Moxley <smox98@uw.edu>
2018-02-03 15:47:33 -0800

    Finish exercise 8

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,36 +1,40 @@
 # Exercise 8: exploring data sets
 
 # Load the dplyr library
-
+library(dplyr)
 
 # Read in the data (from `data/pupulitzer-circulation-data.csv`). Remember to 
 # not treat strings as factors!
-
+pcd <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
 
 # View in the data set. Start to understand what the data set contains
-
+View(pcd)
 
 # Print out the names of the columns for reference
-
+print(colnames(pcd))
 
 # Use the 'str()' function to also see what types of values are contained in 
 # each column (you're looking at the second column after the `:`)
 # Did any value type surprise you? Why do you think they are that type?
-
+str(pcd)
 
 # Add a column to the data frame called 'Pulitzer.Prize.Change` that contains 
 # the difference in the number of times each paper was a winner or finalist 
 # (hereafter "winner") during 2004-2014 and during 1990-2003
-
+pcd <- pcd %>% mutate(Pulitzer.Prize.Change = Pulitzer.Prize.Winners.and.Finalists..2004.2014 - Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What was the name of the publication that has the most winners between 
 # 2004-2014?
-
+most.winners <- pcd %>% filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 == max(Pulitzer.Prize.Winners.and.Finalists..2004.2014))
+print(most.winners$Newspaper)
 
 # Which publication with at least 5 winners between 2004-2014 had the biggest
 # decrease(negative) in daily circulation numbers?
-
+five.winners <- pcd %>% filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 > 4) %>% 
+  filter(Change.in.Daily.Circulation..2004.2013 == min(Change.in.Daily.Circulation..2004.2013))
+print(five.winners$Newspaper)
 
 # An important part about being a data scientist is asking questions. 
 # Write a question you may be interested in about this data set, and then use  
 # dplyr to figure out the answer!
+
